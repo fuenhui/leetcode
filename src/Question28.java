f48bbe639fb350b81716c51fd554e0ef393b6590
fuenhui <fuenhui@163.com>
2021-01-06 20:37:31 +0800

    问题28

---
+++ src/Question28.java
@@ -0,0 +1,12 @@
+/**
+ * https://leetcode-cn.com/problems/implement-strstr/
+ *
+ * @author fuenhui
+ * @date 2021/01/06
+ */
+public class Question28 {
+
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
