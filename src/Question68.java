1a6abb519ad495a4f21921b2950dd1ad9c14e240
fuenhui <fuenhui@163.com>
2021-01-07 15:02:39 +0800

    问题68

---
+++ src/Question68.java
@@ -0,0 +1,12 @@
+/**
+ * https://leetcode-cn.com/problems/sqrtx/
+ *
+ * @author fuenhui
+ * @date 2021/01/07
+ */
+public class Question68 {
+
+    public int mySqrt(int x) {
+        return (int) Math.sqrt(x);
+    }
+}
