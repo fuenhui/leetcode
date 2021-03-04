118c72cd623ba682c6293b420598af4208612625
fuenhui <fuenhui@163.com>
2021-03-04 21:25:25 +0800

    问题709

---
+++ src/Question709.java
@@ -0,0 +1,12 @@
+/**
+ * https://leetcode-cn.com/problems/to-lower-case/
+ *
+ * @author fuenhui
+ * @date 2021/03/04
+ */
+public class Question709 {
+
+    public String toLowerCase(String str) {
+        return str.toLowerCase();
+    }
+}
